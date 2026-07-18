9bf1d256be0a05a83cf96642c2f04501113e834f
dhividharsana777-vsb <dhividharsana777@gmail.com>
2026-07-18 20:12:10 +0530

    Create merge k sorted lists(hard).java

---
+++ merge k sorted lists(hard).java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+class Solution {
+    public ListNode mergeKLists(ListNode[] lists) {
+        
+        if (lists == null || lists.length == 0) return null;
+
+        PriorityQueue<ListNode> pq = new PriorityQueue<>(
+            (a, b) -> a.val - b.val
+        );
+
+        // add all list heads
+        for (ListNode node : lists) {
+            if (node != null) {
+                pq.offer(node);
+            }
+        }
+
+        ListNode dummy = new ListNode(0);
+        ListNode temp = dummy;
+
+        while (!pq.isEmpty()) {
+            ListNode minNode = pq.poll();
+            temp.next = minNode;
+            temp = temp.next;
+
+            if (minNode.next != null) {
+                pq.offer(minNode.next);
+            }
+        }
+
+        return dummy.next;
+    }
+}
