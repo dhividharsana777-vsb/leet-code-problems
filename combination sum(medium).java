88da7bd93ccd6c5f02d1848c7316acd2f8957da0
Dhividharsana <dhividharsana777@gmail.com>
2026-07-23 19:24:06 +0530

    \

---
+++ combination sum(medium).java
@@ -0,0 +1,30 @@
+import java.util.*;
+
+class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        backtrack(candidates, target, 0, new ArrayList<>(), result);
+        return result;
+    }
+
+    private void backtrack(int[] candidates, int target, int start,
+                           List<Integer> temp, List<List<Integer>> result) {
+
+        if (target == 0) {
+            result.add(new ArrayList<>(temp));
+            return;
+        }
+
+        if (target < 0) return;
+
+        for (int i = start; i < candidates.length; i++) {
+            temp.add(candidates[i]);
+
+            // stay at i (reuse allowed)
+            backtrack(candidates, target - candidates[i], i, temp, result);
+
+            // backtrack
+            temp.remove(temp.size() - 1);
+        }
+    }
+}
