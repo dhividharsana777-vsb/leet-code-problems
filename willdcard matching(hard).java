f5c85c020406340d315463df8b50645a6f998691
Dhividharsana <dhividharsana777@gmail.com>
2026-07-25 09:21:23 +0530

    Create willdcard matching(hard).java

---
+++ willdcard matching(hard).java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        int i = 0, j = 0;
+        int starIndex = -1, match = 0;
+
+        while (i < s.length()) {
+
+            // Case 1: Match or '?'
+            if (j < p.length() && 
+               (p.charAt(j) == s.charAt(i) || p.charAt(j) == '?')) {
+                i++;
+                j++;
+            }
+
+            // Case 2: '*'
+            else if (j < p.length() && p.charAt(j) == '*') {
+                starIndex = j;
+                match = i;
+                j++;
+            }
+
+            // Case 3: mismatch but previous '*'
+            else if (starIndex != -1) {
+                j = starIndex + 1;
+                match++;
+                i = match;
+            }
+
+            // Case 4: no match
+            else {
+                return false;
+            }
+        }
+
+        // Check remaining pattern
+        while (j < p.length() && p.charAt(j) == '*') {
+            j++;
+        }
+
+        return j == p.length();
+    }
+}
