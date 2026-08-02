bb7f4d149a38f40e961b36716b37e50f383ad526
Dhividharsana <dhividharsana777@gmail.com>
2026-08-02 15:45:46 +0530

    Create permutation sequence(hard).java

---
+++ permutation sequence(hard).java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public String getPermutation(int n, int k) {
+        List<Integer> numbers = new ArrayList<>();
+        int fact = 1;
+        
+        // Prepare numbers and factorial
+        for (int i = 1; i < n; i++) {
+            fact *= i;
+            numbers.add(i);
+        }
+        numbers.add(n);
+        
+        k = k - 1; // convert to 0-based
+        
+        StringBuilder result = new StringBuilder();
+        
+        while (true) {
+            int index = k / fact;
+            result.append(numbers.get(index));
+            numbers.remove(index);
+            
+            if (numbers.size() == 0) break;
+            
+            k = k % fact;
+            fact = fact / numbers.size();
+        }
+        
+        return result.toString();
+    }
+}
