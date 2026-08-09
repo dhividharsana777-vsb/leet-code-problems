7b9b6c39f7545ed4f8fee2a12f479d2ab6bf15f8
Dhividharsana <dhividharsana777@gmail.com>
2026-08-09 20:31:21 +0530

    Implement pivot index calculation in Solution class

---
+++ find pivot index(easy).java
@@ -0,0 +1,25 @@
+class Solution {
+    public int pivotIndex(int[] nums) {
+        int total = 0;
+
+        // Step 1: Calculate total sum
+        for (int num : nums) {
+            total += num;
+        }
+
+        int leftSum = 0;
+
+        // Step 2: Traverse and check pivot
+        for (int i = 0; i < nums.length; i++) {
+            int rightSum = total - leftSum - nums[i];
+
+            if (leftSum == rightSum) {
+                return i;
+            }
+
+            leftSum += nums[i];
+        }
+
+        return -1;
+    }
+}
